e1fc98098f591450626129c96e0778f2093b2fd3
Ziquan Wang <ziquanwang@MacBook-Pro.attlocal.net>
2026-04-12 10:06:09 -0500

    fixed some dermoscopy param and added missing package to compare modes

--- pyhealth/datasets/dermoscopy.py
+++ pyhealth/datasets/dermoscopy.py
@@ -33,7 +33,7 @@ Data Sources:
             https://www.kaggle.com/datasets/spacesurfer/ph2-dataset
 
 Author:
-    Generated for PyHealth integration of dermoscopic_artifacts datasets.
+    Ziquan Wang
 """
 
 import logging
@@ -119,9 +119,6 @@ class DermoscopyDataset(BaseDataset):
         self,
         root: str,
         datasets: Optional[List[str]] = None,
-        isic2018_metadata_path: Optional[str] = None,
-        ham10000_metadata_path: Optional[str] = None,
-        ph2_metadata_path: Optional[str] = None,
         dataset_name: Optional[str] = None,
         config_path: Optional[str] = None,
         cache_dir: Optional[str] = None,
@@ -134,12 +131,6 @@ class DermoscopyDataset(BaseDataset):
                 raise ValueError(
                     f"Unknown dataset '{ds}'. Must be one of {self.SUPPORTED_DATASETS}"
                 )
-
-        self._metadata_paths = {
-            "isic2018": isic2018_metadata_path,
-            "ham10000": ham10000_metadata_path,
-            "ph2": ph2_metadata_path,
-        }
 
         if config_path is None:
             logger.info("No config path provided, using default config")
